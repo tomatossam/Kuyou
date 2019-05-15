60fcab2d54aa96c08113fc4462d856e38fb76761
tomatossam <853237267@qq.com>
2019-05-15 19:52:45 +0800

    更新获取粉丝列表和获取关注列表2.0

--- src/main/java/com/example/kuyou/entity/User.java
+++ src/main/java/com/example/kuyou/entity/User.java
@@ -1,11 +1,19 @@
 package com.example.kuyou.entity;
 
 public class User {
-
+        private long u_id;
         private String nick;
         private String head;
         private String signature;
         private String isfollowed;
+
+    public long getU_id() {
+        return u_id;
+    }
+
+    public void setU_id(long u_id) {
+        this.u_id = u_id;
+    }
 
     public String getIsfollowed() {
         return isfollowed;
