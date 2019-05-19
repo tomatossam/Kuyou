760391c6c0f7855932474b731db0206043145185
tomatossam <853237267@qq.com>
2019-05-19 22:22:12 +0800

    更新activity/banner接口l_id和l_content

--- src/main/java/com/example/kuyou/entity/Activitysp.java
+++ src/main/java/com/example/kuyou/entity/Activitysp.java
@@ -5,6 +5,24 @@ public class Activitysp {
     public long a_id;
     public String a_cover;
     public String a_slogan;
+    public long l_id;
+    public String l_content;
+
+    public long getL_id() {
+        return l_id;
+    }
+
+    public void setL_id(long l_id) {
+        this.l_id = l_id;
+    }
+
+    public String getL_content() {
+        return l_content;
+    }
+
+    public void setL_content(String l_content) {
+        this.l_content = l_content;
+    }
 
     public String getA_slogan() {
         return a_slogan;
